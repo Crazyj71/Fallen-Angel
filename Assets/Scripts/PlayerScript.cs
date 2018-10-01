3afb9d2caf2cfd3b740bbce54b76302451076a23
Jamie Wyatt <jokolo71@gmail.com>
2018-10-01 12:26:34 -0500

    Added Friction to player when stopped

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,25 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
 
-    public float runSpeed;
+public class PlayerScript : MonoBehaviour {
+    public float friction;
+    public float runForce;
     public float jumpForce;
+    public float maxRunSpeed = 20f;
 
     private Rigidbody2D rb2d;
     private Collider2D c2d;
+  
+    private bool jump = false;
     // Use this for initialization
 
     void Movement()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal, 0);
-        rb2d.AddForce(movement*runSpeed);
+        //IF USER IS PRESSING ARROW KEYS
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)) && rb2d.IsTouchingLayers(Physics2D.AllLayers))
+        {
+            Vector2 velocity = rb2d.velocity;
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            Vector2 movement = new Vector2(moveHorizontal, 0f);
+            float currentspeed = moveHorizontal * rb2d.velocity.x;
+            if (currentspeed < maxRunSpeed) {
+                rb2d.AddForce(movement * runForce);
+            }
+        }
+        else if(rb2d.IsTouchingLayers(Physics2D.AllLayers) && !Input.GetKey(KeyCode.Space))
+        {
+            rb2d.velocity = friction*rb2d.velocity;
+        }
     }
 
     void Jump()
     {
-        rb2d.AddForce(new Vector2(0, 1)*jumpForce);
+        rb2d.AddForce(new Vector2(0f, jumpForce));
+        jump = false;
     }
 
     void Start () {
@@ -29,12 +46,23 @@ public class PlayerScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        Movement();
+	void Update () {
+        transform.rotation = Quaternion.identity;
         if (Input.GetKeyDown(KeyCode.Space) && rb2d.IsTouchingLayers(Physics2D.AllLayers))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+        Movement();
+
+        if (jump)
         {
             Jump();
         }
 
-	}
+    }
 }
