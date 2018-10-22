8ce314a5324f206d7533cf6fa44b356c247225d9
crazyj71 <jokolo71@gmail.com>
2018-10-22 13:33:49 -0500

    changes

---
+++ Assets/Scenes/LoadSceneOnClick.cs
@@ -0,0 +1,16 @@
+﻿//Jamie Wyatt
+//Video Game Design
+//Project 2
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
