c21960b5f3ceb1c4f76b06b7e93f42ca2b119c05
crazyj71 <jokolo71@gmail.com>
2018-10-31 12:56:17 -0500

    Finished HELL PLATS!!!!!

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -10,13 +10,19 @@ public class MovingPlatform : MonoBehaviour
     public float speed = 2.0f;
     private int direction = 1;
     Vector2 movement;
+    private float pos;
+    void Start()
+    {
+        pos = transform.localPosition.x;
+    }
+
     void Update()
     {
-        if (transform.localPosition.x > rightLimit)
+        if (transform.localPosition.x > pos + rightLimit)
         {
             direction = -1;
         }
-        else if (transform.localPosition.x < leftLimit)
+        else if (transform.localPosition.x < pos - leftLimit)
         {
             direction = 1;
         }
